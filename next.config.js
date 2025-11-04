ac083967933d68ed1296d83f3ed8a7f7659d98a8
damienpeuch-dev <peuch.damien24@gmail.com>
2025-11-04 16:15:52 +0100

    fix: redirect apex '/' to www

--- next.config.js
+++ next.config.js
@@ -1,16 +1,22 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   async redirects() {
     return [
+      // 1) Redirection explicite pour la racine
+      {
+        source: '/',
+        has: [{ type: 'host', value: 'pourlestoutpetits.fr' }],
+        destination: 'https://www.pourlestoutpetits.fr',
+        permanent: true,
+      },
+      // 2) Redirection pour tous les chemins
       {
         source: '/:path*',
-        // si l'Host est le domaine APEX, on redirige vers WWW
         has: [{ type: 'host', value: 'pourlestoutpetits.fr' }],
         destination: 'https://www.pourlestoutpetits.fr/:path*',
-        permanent: true
-      }
+        permanent: true,
+      },
     ];
-  }
+  },
 };
-
 module.exports = nextConfig;
