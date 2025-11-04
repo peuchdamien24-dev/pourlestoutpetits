8fce7313bc385b039426170ac058db4cc2b0e660
damienpeuch-dev <peuch.damien24@gmail.com>
2025-11-04 02:31:09 +0100

    fix(auth): add SessionProvider and alias @

---
+++ providers/SessionProvider.tsx
@@ -0,0 +1,13 @@
+﻿"use client";
+
+import { SessionProvider as NextAuthSessionProvider } from "next-auth/react";
+import React from "react";
+
+export default function SessionProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  // Tu peux ajouter des options ici si besoin (refetchInterval, etc.)
+  return <NextAuthSessionProvider>{children}</NextAuthSessionProvider>;
+}
